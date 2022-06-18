ead27e6234f4b3c05565598c58e8e8ff021af185
Kenneth Fang <fangkw42@gmail.com>
2022-06-18 01:53:19 -0400

    HW2 done

--- CS194/Week02/hw/LogAnalysis.hs
+++ CS194/Week02/hw/LogAnalysis.hs
@@ -11,3 +11,34 @@ parseMessage message = case words message of
 
 parse :: String -> [LogMessage]
 parse file = map parseMessage (lines file)
+
+logTime :: LogMessage -> Int
+logTime (Unknown _) = -1
+logTime (LogMessage _ t _) = t
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert logMsg tree = case tree of
+    Leaf -> Node Leaf logMsg Leaf
+    Node left nodeLog right -> case (logTime logMsg) <= logTime (nodeLog) of
+        True -> Node (insert logMsg left) nodeLog right
+        False -> Node left nodeLog (insert logMsg right)
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (x:xs) = insert x (build xs)
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node left logMsg right) = (inOrder left) ++ (logMsg : (inOrder right))
+
+isSevere :: LogMessage -> Bool
+isSevere (LogMessage (Error severity) _ _) = severity >= 50
+isSevere _ = False
+
+getMessage :: LogMessage -> String
+getMessage (LogMessage _ _ msg) = msg
+getMessage (Unknown msg) = msg
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong messages = map getMessage (filter isSevere (inOrder (build messages)))
